e57121e259f953a9be955ee8aed15677d068ab62
nethun5919 <ursula.netherland@bryanuniversity.edu>
2021-04-18 04:46:53 -0700

    add

---
+++ input.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    document.getElementById("Form").submit();
+}
+   
+function myFunction() {
+document.getElementById("myForm").submit();
+}
+
+const form= document.myform
+form.addElementListner("subit", (event) =>{
+    evernt.preventDefault()
+    console.log(form.decision)}
+
